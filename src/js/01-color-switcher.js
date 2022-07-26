aa2e9c079624454784815beee283c7e8a7946030
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-07-26 19:27:53 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startInterval = () => {
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    console.log(bodyRef.style.backgroundColor);
+  }, 1000);
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+};
+
+startBtnRef.addEventListener('click', startInterval);
+
+const stopInterval = () => {
+  clearInterval(intervalId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+};
+stopBtnRef.addEventListener('click', stopInterval);
